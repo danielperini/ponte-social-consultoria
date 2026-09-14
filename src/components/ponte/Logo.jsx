72ee92c02a8832494bb07e421b78841d5224b039
Daniel Perini <daniel@periniprojetos.com.br>
2026-09-14 18:55:50 +0000

    Update logo to transparent PNG format

--- src/components/ponte/Logo.jsx
+++ src/components/ponte/Logo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LOGO_URL =
-  "https://media.base44.com/images/public/6aa331bf5cf4993602fef0a7/6a68fc317_fundotranspartne1.png";
+  "https://media.base44.com/images/public/6aa331bf5cf4993602fef0a7/cf2c3c7ce_WhatsAppImage2026-09-14at1551151.png";
 
 export default function Logo({ theme = "brand", className = "" }) {
   return (
