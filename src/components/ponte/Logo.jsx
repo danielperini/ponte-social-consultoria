76be0512df2cae1aa4d587daa6a9c56f8d2081cc
Daniel Perini <daniel@periniprojetos.com.br>
2026-09-14 20:49:35 +0000

    Atualizar logo para arquivo transparente

--- src/components/ponte/Logo.jsx
+++ src/components/ponte/Logo.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const LOGO_URL = "https://media.base44.com/images/public/6aa331bf5cf4993602fef0a7/a4a38e4dc_WhatsAppImage2026-09-11at0938173.jpeg";
+const LOGO_URL = "https://media.base44.com/images/public/6aa331bf5cf4993602fef0a7/fa3ce29c6_ChatGPTImage14desetde202617_48_09.png";
 
 export default function Logo({ theme = "brand", className = "" }) {
   return (
     <img
       src={LOGO_URL}
       alt="Ponte Social Consultoria"
-      className={`object-contain h-16 lg:h-20 w-auto ${className}`}
+      className={`object-contain h-14 lg:h-20 w-auto ${className}`}
     />
   );
 }
