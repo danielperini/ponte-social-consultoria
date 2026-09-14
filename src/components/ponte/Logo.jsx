afca86d2e61c65e7ebfcf29baf5e5573c4daf6fe
Daniel Perini <daniel@periniprojetos.com.br>
2026-09-14 20:33:48 +0000

    Restaurar logo da Ponte Social no header

--- src/components/ponte/Logo.jsx
+++ src/components/ponte/Logo.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 const LOGO_URL =
-  "https://media.base44.com/images/public/6aa331bf5cf4993602fef0a7/9aea63e71_ChatGPTImage14desetde202617_28_23.png";
+  "https://media.base44.com/images/public/6aa331bf5cf4993602fef0a7/cf2c3c7ce_WhatsAppImage2026-09-14at1551151.png";
 
 export default function Logo({ theme = "brand", className = "" }) {
   return (
     <img
       src={LOGO_URL}
-      alt="SCAMBIO.IA"
-      className={`object-contain h-16 lg:h-20 w-auto ${className}`}
+      alt="Ponte Social"
+      className={`object-contain h-20 lg:h-28 w-auto ${className}`}
     />
   );
 }
